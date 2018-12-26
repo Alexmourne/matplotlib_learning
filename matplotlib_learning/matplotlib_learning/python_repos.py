7ca1831d099e361856477a0a91a7dd4aab144a49
Alexmourne <lixumourne@hotmail.com>
2018-12-26 16:53:01 +0800

    Fix cant decode with 'none' description, freaking shadowsocks.

--- matplotlib_learning/matplotlib_learning/python_repos.py
+++ matplotlib_learning/matplotlib_learning/python_repos.py
@@ -46,12 +46,17 @@ def print_each_dict(repo_dict, repo_dicts):
 names, stars = [], []
 plot_dicts = []
 for repo_dict in repo_dicts:
-    names.append(repo_dict['name'])
+    name = repo_dict['name']
+    names.append(name)
     stars.append(repo_dict['stargazers_count'])
+
+    description = repo_dict['description']
+    if not description:
+        description = "Opps, freaking "+name+" had nothing to describe themself!"
 
     plot_dict = {
         'value': repo_dict['stargazers_count'],
-        'label': repo_dict['description'],
+        'label': description,
         'xlink': repo_dict['html_url'],
         }
     plot_dicts.append(plot_dict)
@@ -74,7 +79,7 @@ chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
-chart.add('', stars)
-#chart.add('', plot_dicts)
+#chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('images\python_repos.svg')
 
