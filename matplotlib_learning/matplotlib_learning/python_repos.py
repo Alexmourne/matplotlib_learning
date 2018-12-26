b2c41789b65e0f4d88405dde34b92aba19996846
Alexmourne <lixumourne@hotmail.com>
2018-12-26 15:23:58 +0800

    >.<

--- matplotlib_learning/matplotlib_learning/python_repos.py
+++ matplotlib_learning/matplotlib_learning/python_repos.py
@@ -4,7 +4,7 @@ from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 #
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
-r= requests.get(url)
+r = requests.get(url)
 print("Status code:", r.status_code)
 
 #
